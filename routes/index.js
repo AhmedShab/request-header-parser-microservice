fe97b7ffb0575b6457797441fdbe9a7a72895b03
AhmedShab <ahmed.vuw@gmail.com>
2016-06-17 21:46:19 +1200

    made the respond sends json obj

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,15 @@ router.get('/api/whoami/', function (req, res) {
 		var ip = req.headers['x-forwarded-for'] ||
 						 req.connection.remoteAddress;
 		var language = req.headers["accept-language"].split(',')[0];
-		var os = req.headers["user-agent"].match(/\((.*?)\)/)[1]
+		var os = req.headers["user-agent"].match(/\((.*?)\)/)[1];
 		// console.log(language.language);
-		res.send(JSON.stringify({
+		var results = [{
 			ipaddress : ip,
 			language: language,
 			system: os
-		}));
+		}];
+
+		return res.json(results);
 	});
 
 module.exports = router;
